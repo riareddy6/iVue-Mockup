34806ce61bdf1fd33b300be17df1b4ce81f28ff1
riareddy6 <ria.bonthu.reddy@gmail.com>
2025-05-04 15:17:24 -0400

    test commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 id="google-logo">
+    <span class="g1">G</span>
+    <span class="o1">o</span>
+    <span class="o2">o</span>
+    <span class="g2">g</span>
+    <span class="l">l</span>
+    <span class="e">e</span>
+  </h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
+/* Import Montserrat font */
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: 'Montserrat', sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 15vh;
 }
+
+/* Google Logo */
+#google-logo {
+  font-size: 87px;  
+  font-weight: 570;
+}
+
+/* Light Mode Logo */
+.g1 { color: #4285F4; }  /* Blue */
+.o1 { color: #EA4335; }  /* Red */
+.o2 { color: #FBBC05; }  /* Yellow */
+.g2 { color: #4285F4; }  /* Blue */
+.l  { color: #34A853; }  /* Green */
+.e  { color: #EA4335; }  /* Red */
 </style>
